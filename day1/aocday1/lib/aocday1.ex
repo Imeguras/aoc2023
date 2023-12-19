f0c01d45b08a3ddc14825d339a6a0e3f9ff30e48
Imeguras <imeguras@cock.li>
2023-12-19 16:25:38 +0000

    Done

--- day1/aocday1/lib/aocday1.ex
+++ day1/aocday1/lib/aocday1.ex
@@ -1,20 +1,53 @@
 defmodule Aocday1 do
-  def start() do
-    read_file
-  end
-  def read_file(function) do
+  def start(_type, _args) do
 
-    File.read!("input.txt")
+    f=read_file()
+    |> Enum.sum()
+
+    IO.puts(f)
+
+    children = [
+
+    ]
+
+
+    opts = [strategy: :one_for_one, name: A.Supervisor]
+    Supervisor.start_link(children, opts)
+
+
+  end
+
+  def read_file() do
+
+    File.read("../input.txt")
+    |> text_parse
+
+
+
+  end
+  defp text_parse({:ok, file}) do
+    file
     |> String.split("\n")
-    |> function()
+    |> Enum.filter(fn x -> x != "" end)
+    |> discard_text
+    |> append_numbers
 
   end
+
+  defp text_parse({:error, reason}) do
+    IO.puts("Error: #{reason}")
+  end
+
   def discard_text(input) do
     input
-    |> Enum.map(fn x -> String.replace(x, "[A-z]*", "") end)
+    |> Enum.map(fn x -> Regex.replace(~r/[^0-9]+/ ,x, "") end)
   end
   def append_numbers(input) do
     input
-    |> Enum.map(fn x -> Integer.parse() end)
+    |> Enum.map(fn x -> Integer.parse(x) end)
+    |> Enum.map(fn {x, _} -> x end)
+
+
+
   end
 end
